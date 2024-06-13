e684dd6185f2953fac1e6faa32512d501f2207dd
Diego Alonso Rubí Salas <43016665+Diegoalru@users.noreply.github.com>
2024-06-13 22:57:33 +0000

    Add metrics tracking for service calls and stock changes in Products service.

---
+++ dotnet-observability/eShopLite/Products/Metrics/ProductsMetrics.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace Products.Metrics;
+
+/// <summary>
+/// Represents a class for tracking metrics related to products.
+/// </summary>
+public class ProductsMetrics
+{
+    private readonly Counter<int> _serviceCalls;
+    private readonly Counter<int> _stockChange;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ProductsMetrics"/> class.
+    /// </summary>
+    /// <param name="meterFactory">The meter factory used to create metrics.</param>
+    public ProductsMetrics(IMeterFactory meterFactory)
+    {
+        var meter = meterFactory.Create("eShopLite.Products");
+        _serviceCalls = meter.CreateCounter<int>(name: "eshoplite.products.service_calls", unit: "{calls}", description: "Number of times the product service is being called to list all products.");
+        _stockChange = meter.CreateCounter<int>(name: "eshoplite.products.stock_change", unit: "{stock}", description: "Amount of stock being changed through the product service.");
+    }
+
+    /// <summary>
+    /// Tracks the number of service calls made to the product service.
+    /// </summary>
+    /// <param name="quantity">The quantity of service calls to track.</param>
+    public void ServiceCalls(int quantity)
+    {
+        _serviceCalls.Add(quantity);
+    }
+
+    /// <summary>
+    /// Tracks the amount of stock being changed through the product service.
+    /// </summary>
+    /// <param name="quantity">The quantity of stock change to track.</param>
+    public void StockChange(int quantity)
+    {
+        _stockChange.Add(quantity);
+    }
+}
--- dotnet-observability/eShopLite/Products/Program.cs
+++ dotnet-observability/eShopLite/Products/Program.cs
@@ -1,13 +1,19 @@
 using Microsoft.EntityFrameworkCore;
 using Products.Data;
 using Products.Endpoints;
+using Products.Metrics;
 
 var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
+
 builder.Services.AddDbContext<ProductDataContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("ProductsContext") ?? throw new InvalidOperationException("Connection string 'ProductsContext' not found.")));
+    options.UseSqlite(config.GetConnectionString("ProductsContext") ?? throw new InvalidOperationException("Connection string 'ProductsContext' not found.")));
 
 // Add observability code here
-builder.Services.AddObservability("Products", builder.Configuration);
+builder.Services.AddObservability("Products", config, ["eShopLite.Products"]);
+
+// Register the metrics service.
+builder.Services.AddSingleton<ProductsMetrics>();
 
 // Add services to the container.
 var app = builder.Build();
